0bdb6f2e9752add3963df6a4b9f866c571ee85bf
marielle1022 <marielle.riveros@gmail.com>
2019-09-27 16:30:43 -0400

    asfs

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -6,8 +6,8 @@ export default function game_init(root) {
   ReactDOM.render(<Starter />, root);
 }
 
-{/* Citation: React tutorial (https://reactjs.org/tutorial/tutorial.html)
-  to learn React and for ideas on structure.*/}
+/* Citation: React tutorial (https://reactjs.org/tutorial/tutorial.html)
+  to learn React and for ideas on structure.*/
 class Starter extends React.Component {
   constructor(props) {
     super(props);
@@ -21,9 +21,10 @@ class Starter extends React.Component {
     var tempTiles = arr.slice();
     while (tempTiles.length > 0) {
       var len = tempTiles.length;
-      {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random used to understand and implement Math.random()*/}
+      /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+      used to understand and implement Math.random()*/
       var index = Math.floor(Math.random() * len);
-      {/*https://www.robinwieruch.de/react-state-array-add-update-remove to setState and cocatenate*/}
+      /*https://www.robinwieruch.de/react-state-array-add-update-remove to setState and cocatenate*/
       var tempValue = tempTiles[index];
       this.setState(state => {
         const randomTiles = state.list.concat(state.value);
@@ -42,31 +43,31 @@ class Starter extends React.Component {
   render() {
     return (
       randomizeTiles(this.state.originalTiles);
-      {/* TODO: delete console log*/}
+      /* TODO: delete console log*/
       console.log(this.state.randomTiles);
       <div>
-        {/* Create 4x4 row*/}
+        /* Create 4x4 row*/
         <div className="board-row">
           {this.renderTile(0)}
           {this.renderTile(1)}
           {this.renderTile(2)}
           {this.renderTile(3)}
         </div>
-        {/* Create 4x4 row*/}
+        /* Create 4x4 row*/
         <div className="board-row">
           {this.renderTile(4)}
           {this.renderTile(5)}
           {this.renderTile(6)}
           {this.renderTile(7)}
         </div>
-        {/* Create 4x4 row*/}
+        /* Create 4x4 row*/
         <div className="board-row">
           {this.renderTile(8)}
           {this.renderTile(9)}
           {this.renderTile(10)}
           {this.renderTile(11)}
         </div>
-        {/* Create 4x4 row*/}
+        /* Create 4x4 row*/
         <div className="board-row">
           {this.renderTile(12)}
           {this.renderTile(13)}
@@ -86,7 +87,7 @@ class Tile extends React.Component{
   render() {
     return(
       <button className="tile">
-      {/*Need to fill in*/}
+      /*Need to fill in*/
       </button>
     );
   }
