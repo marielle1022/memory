c0b4d77721c015c31c9d4513e9427deae6e7daea
marielle1022 <marielle@debian1022>
2019-09-27 15:52:55 -0400

    adding all

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -6,40 +6,85 @@ export default function game_init(root) {
   ReactDOM.render(<Starter />, root);
 }
 
+// Citation: React tutorial (https://reactjs.org/tutorial/tutorial.html) 
+// to learn React and for ideas on structure.
 class Starter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    this.state = {
+	    originalTiles: ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F',
+		    'G', 'G', 'H', 'H'],
+	    randomTiles: []
+    };
   }
-  hax(_ev) {
-    alert("hax!");
-  }
-
+	randomizeTiles(arr){
+		var tempTiles = arr.slice();
+		while (tempTiles.length > 0) {
+			var len = tempTiles.length;
+			{/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random used to understand and implement Math.random()*/}
+			var index = Math.floor(Math.random() * len);
+			{/*https://www.robinwieruch.de/react-state-array-add-update-remove to setState and cocatenate*/}
+			var tempValue = tempTiles[index];
+			this.setState(state => {
+				const randomTiles = state.list.concat(state.value);
+				return {
+					randomTiles,
+					value: tempValue,
+				};
+			
+		};
+				tempTiles = tempTiles.filter(tempTiles[index]);
+	};
+	}
+	renderTile(i){
+		return <Tile />;
+	}
   render() {
-    let button = <div className="column">
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
 
-    let blank = <div className="column">
-      <p>Nothing.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
-    }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
-    }
+	return (
+		randomizeTiles(this.state.originalTiles);
+		{/* TODO: delete console log*/}
+		console.log(this.state.randomTiles);
+			<div>
+				<div className="status">{status}</div>
+		{/* Create 4x4 row*/}
+				<div className="board-row">
+					{this.renderTile(0)}
+					{this.renderTile(1)}
+					{this.renderTile(2)}
+					{this.renderTile(3)}
+				</div>
+				<div className="status">{status}</div>
+			{/* Create 4x4 row*/}	
+				<div className="board-row">
+					{this.renderTile(4)}
+					{this.renderTile(5)}
+					{this.renderTile(6)}
+					{this.renderTile(7)}
+				</div>
+				<div className="status">{status}</div>
+				{/* Create 4x4 row*/}
+				<div className="board-row">
+					{this.renderTile(8)}
+					{this.renderTile(9)}
+					{this.renderTile(10)}
+					{this.renderTile(11)}
+				</div>			
+				<div className="status">{status}</div>
+				{/* Create 4x4 row*/}
+				<div className="board-row">
+					{this.renderTile(12)}
+					{this.renderTile(13)}
+					{this.renderTile(14)}
+					{this.renderTile(15)}
+				</div>
+			</div>
+		);	
   }
 }
 
-// Citation: React tutorial (https://reactjs.org/tutorial/tutorial.html) for ideas on structure.
+// Citation: React tutorial (https://reactjs.org/tutorial/tutorial.html) 
+// to learn React and for ideas on structure.
 // This class represents the basic tiles on the board
 // TODO: may need to change it from a class to a function
 class Tile extends React.Component{
@@ -52,53 +97,3 @@ class Tile extends React.Component{
 	}
 }
 
-// Citation: React tutorial (https://reactjs.org/tutorial/tutorial.html) for ideas on structure.
-// This class creates the board setup
-class Board extends React.Component {
-	renderTile(i) {
-		return <Tile />;
-	}
-
-	// Main render for Board class
-	render() {
-		// Hold the status of which player goes next
-		const status = 'Next player: ';
-
-		return (
-			<div>
-				<div className="status">{status}</div>
-				// Create 4x4 row
-				<div className="board-row">
-					{this.renderTile(0)}
-					{this.renderTile(1)}
-					{this.renderTile(2)}
-					{this.renderTile(3)}
-				</div>
-				<div className="status">{status}</div>
-				// Create 4x4 row
-				<div className="board-row">
-					{this.renderTile(4)}
-					{this.renderTile(5)}
-					{this.renderTile(6)}
-					{this.renderTile(7)}
-				</div>
-				<div className="status">{status}</div>
-				// Create 4x4 row
-				<div className="board-row">
-					{this.renderTile(8)}
-					{this.renderTile(9)}
-					{this.renderTile(10)}
-					{this.renderTile(11)}
-				</div>			
-				<div className="status">{status}</div>
-				// Create 4x4 row
-				<div className="board-row">
-					{this.renderTile(12)}
-					{this.renderTile(13)}
-					{this.renderTile(14)}
-					{this.renderTile(15)}
-				</div>
-			</div>
-		);
-	}
-}
