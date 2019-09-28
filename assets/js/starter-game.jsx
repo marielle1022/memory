bf57cc7486fd5706027a2cbc36e5d73c8c79db34
marielle1022 <marielle.riveros@gmail.com>
2019-09-27 21:08:37 -0400

    changing i to indeex

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -38,7 +38,7 @@ class Starter extends React.Component {
       let tempValue = tempTiles[index];
 
       console.log(tempValue);
-      tempTiles = tempTiles.concat(tempTiles.slice(0, i), tempTiles.slice(i + 1))
+      tempTiles = tempTiles.concat(tempTiles.slice(0, index), tempTiles.slice(index + 1))
       console.log("After removal");
       console.log(tempTiles);
       /*
